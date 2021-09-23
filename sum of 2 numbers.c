ff4e52566a1b1cfb36ffa0e06278ee3c90a2aa9c
Hemanth040304 <91045353+Hemanth040304@users.noreply.github.com>
2021-09-23 16:32:53 +0530

    Add files via upload

---
+++ sum of 2 numbers.c
@@ -0,0 +1,10 @@
+/* C Progam to find Sum of two numbers */
+#include<stdio.h>
+void main()
+{
+	//Declaration
+	int number1=-1234567890 ,number2=-1234567890 ;
+	
+	//Output
+	printf("%u,",number1+number2);
+}
