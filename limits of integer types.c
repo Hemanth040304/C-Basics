f7c71aa304324967c85a1f3f620168fd3a100157
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2021-10-14 11:37:51 +0530

    Update limits of integer types.c

--- limits of integer types.c
+++ limits of integer types.c
@@ -4,10 +4,10 @@
 void main()
 {
 	//Output
-	printf("Short: %hd to %hd\n,",SHRT_MIN,SHRT_MAX);
-	printf("Unsigned Short: %hu to %hu\n,",0,USHRT_MAX);
-	printf("Int : %d to %d\n,",INT_MIN,INT_MAX);//Long and Int are of same range
-	printf("Unsigned int : %u to %u\n,",0,UINT_MAX);
-	printf("Long Long :%lld to %lld\n,",LONG_LONG_MIN,LONG_LONG_MAX);
+	printf("Short: %hd to %hd,\n",SHRT_MIN,SHRT_MAX);
+	printf("Unsigned Short: %hu to %hu,\n",0,USHRT_MAX);
+	printf("Int : %d to %d,\n",INT_MIN,INT_MAX);//Long and Int are of same range
+	printf("Unsigned int : %u to %u,\n",0,UINT_MAX);
+	printf("Long Long :%lld to %lld,\n",LONG_LONG_MIN,LONG_LONG_MAX);
 	printf("Unsigned Long Long : %llu to %llu,",0,ULONG_LONG_MAX);
 }
