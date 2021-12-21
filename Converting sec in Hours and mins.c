49bac80d6e0fcbc735aa86a99df719a59e800068
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2021-12-21 11:51:11 +0530

    Add files via upload

---
+++ Converting sec in Hours and mins.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int m,h,rm,rh,s;
+	printf("Enter time in seconds.\n");
+	scanf("%d",&s);
+	h=s/3600,rh=s%3600,m=rh/60,rm=rh%60;
+	printf("Hours = %d \nMinutes = %d \nSeconds = %d",h,m,rm);
+}
